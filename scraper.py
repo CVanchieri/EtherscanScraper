0ad92fe0c12b841cb7792ddd46e08385bb9cec83
Charles Vanchieri <cvanchieri6@charless-mbp.lan>
2021-10-26 17:16:56 -0700

    program now scrapes nfts held, token hash, and quantities of nfts.

--- scraper.py
+++ scraper.py
@@ -3,8 +3,6 @@ import requests
 import cloudscraper
 import pandas as pd
 
-
-
 user_input = "0x5FFA235A2478A1e3E1b01CC1EE968Bee915351AF"
 scraper = cloudscraper.create_scraper()
 
@@ -20,19 +18,50 @@ url_main = scraper.get('https://etherscan.io/address/0x5FFA235A2478A1e3E1b01CC1E
 hash_scan = BeautifulSoup(url_main.text, 'lxml')
 hash_title = hash_scan.title.text
 print("-- hash main title --")
-print(hash_title)
+print(hash_title.strip())
+
 # get hash eth balance
 overview = hash_scan.find('div', class_='row mb-4')
 body = overview.find('div', class_='card-body')
 balance_eth = body.select_one('div:nth-child(1)')
 hash_eth_balance = balance_eth.text
+split_string = hash_eth_balance. split(":", 1)
+eth_balance = split_string[1]
 print("-- hash eth balance --")
-print(hash_eth_balance)
+print(eth_balance.strip())
+
 # get hash usd balance
 balance_usd = body.select_one('div:nth-child(3)')
 hash_usd_balance = balance_usd.text
+split_string = hash_usd_balance. split(":", 1)
+usd_balance = split_string[1]
 print("-- hash usd balance --")
-print(hash_usd_balance)
+print(usd_balance.strip())
+
+### token/nft scraper ###
+print("-- eth token/nft holdings --")
+nft_body = body.find('ul', class_='list list-unstyled mb-0')
+
+nfts = {}
+nft_count = 0
+for li in nft_body.find_all('li'):
+    for a in li.find_all('a'):
+        nft = {}
+        hash = a['href']
+        new_hash = hash.replace('/token/', '')
+        nft['hash'] = new_hash
+        name_quantity = a.div.text
+        split_string = name_quantity. split(")", 1)
+        name = split_string[0]+')'
+        nft['name'] = name
+        quantity = ''.join(i for i in split_string[1] if i.isdigit())
+        nft['quantity'] = quantity
+        nft_count = nft_count + 1
+        nfts[nft_count] = nft
+
+# create a dataframe 
+df_tokens = pd.DataFrame.from_dict(nfts, orient='index')
+print(df_tokens.head())
 
 ## token overview scraper ###
 url_tokens = scraper.get('https://etherscan.io/tokenholdings?a=0x5FFA235A2478A1e3E1b01CC1EE968Bee915351AF')
@@ -54,13 +83,15 @@ token_overview = token_body.find('div', class_='container space-bottom-2')
 token_usd_networth = token_overview.find('div', class_='row mx-gutters-md-2').div
 print("-- token usd total networth --")
 token_usd_networth_total = token_usd_networth.text
-print(token_usd_networth_total)
+split_string = token_usd_networth_total. split("$", 1)
+usd_networth_total = '$' + split_string[1]
+print(usd_networth_total.strip())
 
 # get token eth balance
 token_eth_networth = token_overview.find('div', class_='col-md col-md-auto u-ver-divider u-ver-divider--left u-ver-divider--none-md mb-md-4').div
 print("-- token eth total networth --")
 token_eth_networth_total = token_eth_networth.text
-print(token_eth_networth_total)
+print(token_eth_networth_total.strip())
 
 # get token assets
 token_asssets_overview = token_overview.find('div', id='assets-wallet')
@@ -87,7 +118,8 @@ for td in token_asssets_table.find_all('tr'):
     token_count = token_count + 1
 # print(tokens)
 df_tokens = pd.DataFrame.from_dict(tokens, orient='index')
-print(df_tokens.head(10))
+print(df_tokens.head())
+
 ### hash transaction scraper ###
 print('-- transactions --')
 page_count = 0
@@ -130,15 +162,24 @@ while page_count >= 0:
             transactions[transaction_count] = transaction
             transaction_count = transaction_count + 1
         page_count = page_count + 1
-        # print(f'page #{page_count}')
     else:
-        print('no more')
         break
 
-print(f'transaction page #{page_count}')
-print(f'transaction total #{transaction_count}')
-# print(transactions[0])
+print(f'number of transaction pages #{page_count}')
+print(f'number of transactions total #{transaction_count}')
 
 df_transactions = pd.DataFrame.from_dict(transactions, orient='index')
-print(df_transactions.head(10))
 
+df_transactions.eth_value = df_transactions.eth_value.str.rstrip(' Ether')
+df_transactions.hash = df_transactions.hash.astype(object)
+df_transactions.method = df_transactions.method.astype(object)
+df_transactions.block = df_transactions.block.astype(int)
+df_transactions.age = df_transactions.age.astype(object)
+df_transactions.sender = df_transactions.sender.astype(object)
+df_transactions.direction = df_transactions.direction.astype(object)
+df_transactions.reciever = df_transactions.reciever.astype(object)
+df_transactions.eth_value = df_transactions.eth_value.astype(float)
+df_transactions.eth_fee = df_transactions.eth_fee.astype(float)
+print(df_transactions.head())
+
+# df_transactions.to_csv('df_transactions.csv',index=False)
